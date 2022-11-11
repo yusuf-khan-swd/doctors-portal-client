878d327690bb6ca72e86b7cd32403fefcd8f69fe
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-11 23:19:32 +0600

    Create Basic Component For Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+    <div>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>Footer Goes Here</h2>
+    </div>
+  );
+};
+
+export default Footer;
