f378c1c83088b6ea8c9bfe1a60d576fe022af6f3
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-12 22:08:34 +0600

    fixed info card in mobile view

--- src/Pages/Home/InfoCards/InfoCard.js
+++ src/Pages/Home/InfoCards/InfoCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 const InfoCard = ({ card }) => {
   const { icon, title, description, bgClass } = card;
   return (
-    <div className={`card card-side shadow-xl text-white p-6 ${bgClass}`}>
+    <div className={`card md:card-side shadow-xl text-white p-6 ${bgClass}`}>
       <figure>
         <img src={icon} alt="Movie" />
       </figure>
-      <div className="card-body">
+      <div className="card-body p-0 md:p-8">
         <h2 className="card-title"> {title} </h2>
         <p> {description} </p>
       </div>
