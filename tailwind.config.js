a65492d32048a13d39024b1bcc5ca3fcc699f121
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-12 15:50:49 +0600

    added our theme color as daisyUI theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,17 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF"
+        }
+      }
+    ]
+  }
 }
