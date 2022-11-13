1f26ef15e69fc31acfbfcaa4c4dd4fe6d6998991
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-14 00:58:11 +0600

    added react form to handle form and added a login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+
   return (
-    <div>
-      <h2>This is login Page</h2>
+    <div className='h-[400px] flex justify-center items-center'>
+      <div>
+        <h2 className='text-xl text-center'>Login</h2>
+        <form onSubmit={handleSubmit(data => setData(JSON.stringify(data)))}>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input {...register('email')} type="text" className="input input-bordered w-full max-w-xs" />
+          </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input {...register('password')} type="password" className="input input-bordered w-full max-w-xs" />
+          </div>
+          <Link className='link' to='/'>Forgot Password?</Link>
+          <div className="form-control mt-3">
+            <input className='btn' type="submit" value="login" />
+          </div>
+        </form>
+        <p> {data} </p>
+      </div>
     </div>
   );
 };
