6837e9e0f4cfc0fe57793ba905777f2ecf336824
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-13 13:13:38 +0600

    added react day picker and show the date user select

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <header>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} alt='Dentist Chair' className="max-w-sm rounded-lg shadow-2xl" />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <DayPicker
+              mode='single'
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            ></DayPicker>
+            <p>You have Selected {format(selectedDate, 'PP')} </p>
           </div>
         </div>
       </div>
