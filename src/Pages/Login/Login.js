820a985642b445a04c38fbfd8350098cc5a1e026
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-15 00:04:55 +0600

    added email and password validation and format with prettier

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,41 +1,75 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <div className='h-[400px] flex justify-center items-center'>
-      <div className='w-96 shadow-lg rounded-2xl p-7'>
-        <h2 className='text-xl text-center'>Login</h2>
+    <div className="h-[400px] flex justify-center items-center">
+      <div className="w-96 shadow-lg rounded-2xl p-7">
+        <h2 className="text-xl text-center">Login</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input {...register('email')} type="text" className="input input-bordered w-full max-w-xs" />
+            <input
+              {...register("email", { required: "Email Address is required" })}
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.email && (
+              <p className="text-red-600"> {errors?.email?.message} </p>
+            )}
           </div>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input {...register('password')} type="password" className="input input-bordered w-full max-w-xs" />
+            <input
+              {...register(
+                "password",
+                { required: "Password is required" },
+                {
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 character or more.",
+                  },
+                }
+              )}
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.password && (
+              <p className="text-red-600"> {errors?.password?.message} </p>
+            )}
             <label className="label hover:link">
               <span className="label-text">Forgot Password?</span>
             </label>
           </div>
           <div className="form-control my-3">
-            <input className='btn' type="submit" value="login" />
+            <input className="btn" type="submit" value="login" />
           </div>
         </form>
-        <p>New to doctors portal? <Link to='/signup' className='text-secondary'>Create new account</Link> </p>
+        <p>
+          New to doctors portal?{" "}
+          <Link to="/signup" className="text-secondary">
+            Create new account
+          </Link>{" "}
+        </p>
         <div className="divider">OR</div>
-        <button className='btn btn-outline btn-accent w-full'>Continue with google</button>
+        <button className="btn btn-outline btn-accent w-full">
+          Continue with google
+        </button>
       </div>
     </div>
   );
