8e9351e2aaeb932046f01e3808afa865d25c3236
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-14 23:09:46 +0600

    use react hook form to get the input value and design the login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+
+  const onSubmit = data => {
+    console.log(data);
+  };
 
   return (
     <div className='h-[400px] flex justify-center items-center'>
-      <div>
+      <div className='w-96 shadow-lg rounded-2xl p-7'>
         <h2 className='text-xl text-center'>Login</h2>
-        <form onSubmit={handleSubmit(data => setData(JSON.stringify(data)))}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Email</span>
@@ -22,13 +25,17 @@ const Login = () => {
               <span className="label-text">Password</span>
             </label>
             <input {...register('password')} type="password" className="input input-bordered w-full max-w-xs" />
+            <label className="label hover:link">
+              <span className="label-text">Forgot Password?</span>
+            </label>
           </div>
-          <Link className='link' to='/'>Forgot Password?</Link>
-          <div className="form-control mt-3">
+          <div className="form-control my-3">
             <input className='btn' type="submit" value="login" />
           </div>
         </form>
-        <p> {data} </p>
+        <p>New to doctors portal? <Link to='/signup' className='text-secondary'>Create new account</Link> </p>
+        <div className="divider">OR</div>
+        <button className='btn btn-outline btn-accent w-full'>Continue with google</button>
       </div>
     </div>
   );
