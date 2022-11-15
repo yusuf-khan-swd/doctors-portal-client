d2ad6cf5dd6c6cd8a178dd4093747290546d8a8f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-15 15:42:13 +0600

    set a observer

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,11 +1,12 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../firebase/firbase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password)
@@ -15,7 +16,15 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  const authInfo = { createUser, signIn };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = { user, createUser, signIn };
 
   return (
     <AuthContext.Provider value={authInfo}>
