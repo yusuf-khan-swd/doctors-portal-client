14d6472e66b066f9e19de6977730d25344263790
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-19 13:39:16 +0600

    if user is not admin do not show all user route btn

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+  const { user } = useContext(AuthContext);
+
+  const [isAdmin] = useAdmin(user?.email);
+
   return (
     <div>
       <Navbar></Navbar>
@@ -15,7 +21,11 @@ const DashboardLayout = () => {
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100 text-base-content">
             <li><Link to='/dashboard'>My Appointment</Link></li>
-            <li><Link to='/dashboard/users'>All Users</Link></li>
+            <li>
+              {
+                isAdmin && <Link to='/dashboard/users'>All Users</Link>
+              }
+            </li>
           </ul>
         </div>
       </div>
---
+++ src/hooks/useAdmin.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useAdmin = email => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/admin/${email}`)
+      .then(res => res.json())
+      .then(data => {
+        setIsAdmin(data.isAdmin)
+      })
+  }, [email]);
+
+  return [isAdmin];
+};
+
+export default useAdmin;
