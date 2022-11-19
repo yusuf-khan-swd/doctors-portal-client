f96c77c629fc3e0c580036cab17ea70d8a209d8b
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-20 00:40:21 +0600

    added different spinner

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className='h-screen flex justify-center items-center'>
-      <button className="btn btn-square loading"></button>
+      <button className="btn btn-secondary text-white loading">Loading</button>
     </div>
   );
 };
