b77cb9a8163f9a54dfdbfe4cb81c2014003606b7
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-19 11:41:52 +0600

    created all users route in dashboard layout

---
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllUsers = () => {
+  return (
+    <div>
+      <h2 className='text-2xl font-semibold mb-5'>All Users</h2>
+    </div>
+  );
+};
+
+export default AllUsers;
