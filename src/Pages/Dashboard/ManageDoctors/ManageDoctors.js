b1a5d5e276e9646e7e9b9a9db391f30e861dccdd
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-20 13:44:28 +0600

    load doctors

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ManageDoctors = () => {
+  const { data: doctors = [] } = useQuery({
+    queryKey: ['doctors'],
+    queryFn: async () => {
+      const res = await fetch('http://localhost:5000/doctors');
+      const data = await res.json();
+      return data;
+    }
+  });
+
   return (
     <div>
-      <h2>Manage Doctors</h2>
+      <h2 className='text-2xl mb-6'>Manage Doctors</h2>
     </div>
   );
 };
