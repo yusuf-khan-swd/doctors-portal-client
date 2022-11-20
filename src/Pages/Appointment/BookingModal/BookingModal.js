1847d95eb2091a10fd2bdabda0c124c8e4d0526e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-20 19:45:04 +0600

    added price for bookings

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { AuthContext } from "../../../contexts/AuthProvider";
 
 const BookingModal = ({ treatment, setTreatment, selectedDate, refetch }) => {
-  const { name: treatmentName, slots } = treatment;
+  const { name: treatmentName, slots, price } = treatment;
   const date = format(selectedDate, 'PP');
   const { user } = useContext(AuthContext);
 
@@ -22,7 +22,8 @@ const BookingModal = ({ treatment, setTreatment, selectedDate, refetch }) => {
       patient: name,
       slot,
       email,
-      phone
+      phone,
+      price
     };
 
     fetch('http://localhost:5000/bookings', {
