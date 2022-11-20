44b1d06739996fdf6734843ac39ec45ceb3d5aea
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-20 14:00:05 +0600

    created a table to show all doctors

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -13,7 +13,39 @@ const ManageDoctors = () => {
 
   return (
     <div>
-      <h2 className='text-2xl mb-6'>Manage Doctors</h2>
+      <h2 className='text-2xl mb-6'>Manage Doctors: {doctors?.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Specialty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              doctors.map((doctor, index) => <tr key={doctor._id}>
+                <th> {(index + 1) < 10 ? ('0' + (index + 1)) : index + 1} </th>
+                <td>
+                  <div className="avatar">
+                    <div className="w-24 rounded-full">
+                      <img src={doctor.image} alt="" />
+                    </div>
+                  </div>
+                </td>
+                <td>{doctor.name}</td>
+                <td>{doctor.email}</td>
+                <td>{doctor.specialty}</td>
+                <td>Blue</td>
+              </tr>)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
