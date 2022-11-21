e1cd73cd441b2d5e6768016a9c64fefa7449b062
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-22 00:04:27 +0600

    remove admin route for payment

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -61,7 +61,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '/dashboard/payment/:id',
-        element: <AdminRoute><Payment></Payment></AdminRoute>,
+        element: <Payment></Payment>,
         loader: ({ params }) => fetch(`https://doctors-portal-server-alpha.vercel.app/bookings/${params.id}`)
       },
     ]
