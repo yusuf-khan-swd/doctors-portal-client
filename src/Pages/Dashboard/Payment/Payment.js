f36bf76e6b3e8d06642b1e476ea015c6a298d8be
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-21 12:22:31 +0600

    added checkoutForm

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,8 @@
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
   const booking = useLoaderData();
